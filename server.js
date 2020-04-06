a2596f4390bcbbf26802607628175ab8ce3b1961
Spencer Wood <spencer.wood@me.com>
2020-04-05 23:23:11 -0500

    added node modules to the repo, server.js file and wrote the mongoose and exporess functions into the server file

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+//Port for the application to run
+const PORT = process.env.PORT || 3000
+//variable app referring to the express service
+const app = express();
+
+app.use(logger('dev'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+//mongoose connect string, using the localhost 'fitnesstracker'
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/fitnesstracker', { useNewUrlParser: true });
+
+//Post Route
+
+
+//Validation server is running on correct port
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
