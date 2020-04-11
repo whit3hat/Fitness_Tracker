9d7a1a7d80d91a0ff02006cda6cc48bc75edb4c7
Spencer Wood <spencer.wood@me.com>
2020-04-10 22:35:02 -0500

    added the routes to the server.js file, html routes are working

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const logger = require('morgan');
 const mongoose = require('mongoose');
-//Port for the application to run
-const PORT = process.env.PORT || 3000
+
 //variable app referring to the express service
 const app = express();
+//Port for the application to run
+const PORT = process.env.PORT || 3000
+var db = require('./models');
 
 app.use(logger('dev'));
 
@@ -15,7 +17,9 @@ app.use(express.static('public'));
 //mongoose connect string, using the localhost 'fitnesstracker'
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/fitnesstracker', { useNewUrlParser: true });
 
-//Post Route
+//Routes
+require('./routes/apiRoutes.js')(app);
+require('./routes/htmlRoutes.js')(app);
 
 
 //Validation server is running on correct port
