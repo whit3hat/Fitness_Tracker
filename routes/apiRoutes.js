06a8ee9cf77c1f6a8bd0dfce3407491e54acd581
Spencer Wood <spencer.wood@me.com>
2020-04-11 18:22:00 -0500

    created routes for api

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -14,4 +14,32 @@ router.post("/api/workouts", ({ body }, res) => {
     });
 });
 
+router.get('/api/workouts', (req, res) => {
+    Workout.find({})
+    .sort({ date: -1 })
+    .then((result) => {
+        result.forEach((exercise) => {
+            let total = 0;
+            workout.exercises.forEach((exercise) => {
+             total += exercise.duration;
+            });
+        workout.totalDuration = total;
+    });
+    res.json(result);
+})
+.catch((err) => {
+    res.status(404).json(err);
+    });
+});
+
+router.get('/api/workouts/range', (req, res) => {
+    Workout.find({})
+    .sort({ data: -1 })
+    .then((result) => {
+        res.json(result);
+    })
+    .catch((err) => {
+        res.status(404).json(err);
+    });
+});
 module.exports = router;
