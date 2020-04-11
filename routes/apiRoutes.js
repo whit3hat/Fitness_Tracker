1190470d4279f9d69887d1caa60c642180900a44
Spencer Wood <spencer.wood@me.com>
2020-04-10 22:27:36 -0500

    completed the api route for the api/workouts/range call needed for stats.js file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,11 +1,11 @@
 var db = require('../models');
 
 module.exports = function(app) {
-    // app.get('', function(req, res) {
-    //     db.Workout.find({}).then(function(dbWorkout) {
-    //         res.json();
-    //     });
-    // });
+    app.get('/api/workouts/range', function(req, res) {
+        db.Workout.find({}).then(function(dbWorkout) {
+            res.json(dbWorkout);
+        });
+    });
 
     app.get('/api/workouts', (req, res) => {
       db.Workout.find({})
