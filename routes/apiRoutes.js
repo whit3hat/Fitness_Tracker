47eeee7ced8ec70625d8435660edfa4ab995a31f
Spencer Wood <spencer.wood@me.com>
2020-04-11 17:44:38 -0500

    modified server.js for the api routes and removed the reference to the DB

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,15 +1,23 @@
 var db = require('../models');
+const router = require('express').Router();
+// module.exports = function(app) {
+//     app.get('/api/workouts/range', function(req, res) {
+//         db.Workout.find({}).then(function(dbWorkout) {
+//             res.json(dbWorkout);
+//         });
+//     });
 
-module.exports = function(app) {
-    app.get('/api/workouts/range', function(req, res) {
-        db.Workout.find({}).then(function(dbWorkout) {
-            res.json(dbWorkout);
-        });
-    });
+//     app.post('/api/workouts', (req, res) => {
+//       db.Workout.create(req.body).then(function(dbWorkout) {
+//           res.json(dbWorkout)
+//       });
+//     });
+// };
 
-    app.post('/api/workouts', (req, res) => {
-      db.Workout.create(req.body).then(function(dbWorkout) {
-          res.json(dbWorkout)
-      });
+
+router.post("/api/workouts", ({ body }, res) => {
+    Workout.create(body)
+    .then(dbWorkout => {
+        res.status(400).json(err);
     });
-};
+});
--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const express = require('express');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 
-//variable app referring to the express service
-const app = express();
+
 //Port for the application to run
 const PORT = process.env.PORT || 3000
-var db = require('./models');
+
+//variable app referring to the express service
+const app = express();
 
 app.use(logger('dev'));
 
@@ -14,12 +15,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static('public'));
+
 //mongoose connect string, using the localhost 'fitnesstracker'
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/fitnesstracker', { useNewUrlParser: true });
 
 //Routes
-require('./routes/apiRoutes.js')(app);
-require('./routes/htmlRoutes.js')(app);
+app.use(require("./routes/apiRoutes.js"));
+app.use(require("./routes/htmlRoutes.js"));
 
 
 //Validation server is running on correct port
